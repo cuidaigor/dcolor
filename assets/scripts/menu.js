73a8d0f085693b2ebf8bd69f09153c7914aa6ff5
Igor Lee <srleehbb@gmail.com>
2021-04-21 15:33:06 -0300

    Update no menu

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,8 +1,9 @@
 let show = true;
+let toggleClass = false;
 
 const menuSection = document.querySelector(".menu-section");
 const menuToggle = menuSection.querySelector(".menu-toggle");
-const menuItem = menuSection.querySelector(".menu-item");
+const menuItem = document.querySelectorAll(".menu-item");
 
 menuToggle.addEventListener("click", () => {
 
@@ -10,4 +11,19 @@ menuToggle.addEventListener("click", () => {
 
   menuSection.classList.toggle("on", show);
   show = !show;
+});
+
+menuItem.forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+
+    toggleClass = menuSection.classList.contains("on");
+
+    if(toggleClass){
+      document.body.style.overflow = "initial";
+
+      menuSection.classList.remove("on");
+      show = true;
+    }
+
+  });
 });
